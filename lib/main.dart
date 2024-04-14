374fae3b0f9c6c82a93ebb687318e4969ffd9a4a
2202011 <2202011@utrivieramaya.edu.mx>
2024-04-13 22:22:57 -0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firestore/view/form_task.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -60,7 +61,10 @@ class _MyHomePageState extends State<MyHomePage> {
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print('hola');
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AddNewTaskScreen()),
+          );
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
